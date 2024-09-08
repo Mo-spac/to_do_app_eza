5a5b462379557c6a4baf7e29d948f093ef197a70
Mohammed Samy <mom118580@gmail.com>
2024-09-08 09:23:55 -0700

    App Assets

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,32 +1,8 @@
 class AppAssets {
-  static const String appName = "UpTo do";
-  static const String skipName = "Skip";
-  static const String nextName = "Next";
-  static const String backName = "Back";
-  static const String getStartedName = "Get Started";
-  static const String onBoardingSubTitleOne =
-      'You can easily manage all of your daily tasks in DoMe for free';
-  static const String onBoardingTitleTwo = 'Create daily routine';
-  static const String onBoardingSubTitleTwo =
-      'In Uptodo  you can create your personalized routine to stay productive';
-  static const String onBoardingTitleThree = 'Organize your tasks';
-  static const String onBoardingSubTitleThree =
-      'You can organize your daily tasks by adding your tasks into separate categories';
-  static const String today = 'Today';
-  static const String noTaskTitle = 'What do you want to do today?';
-  static const String noTaskSubTitle = 'Tap + to add your tasks';
-  static const String addTask = 'Add Task';
-  static const String tilte = 'Tilte';
-  static const String tilteHint = 'Enter title here';
-  static const String note = 'Note';
-  static const String notehint = 'Enter note here';
-  static const String date = 'Date';
-  static const String startTime = 'Start Time';
-  static const String endTime = 'End Time';
-  static const String createTask = 'Create task';
-  static const String toDo = 'TODO';
-  static const String completed = 'COMPLETED';
-  static const String taskCompleted = 'Task Completed';
-  static const String deleteTask = 'Delete Task';
-  static const String cancel = 'Cancel';
+  static const String basePath = "assets/images/";
+  static const String logo = "${basePath}app_logo.png";
+  static const String on1 = "${basePath}on1.png";
+  static const String on2 = "${basePath}on2.png";
+  static const String on3 = "${basePath}on3.png";
+  static const String noTasks = "${basePath}no_tasks.png";
 }
