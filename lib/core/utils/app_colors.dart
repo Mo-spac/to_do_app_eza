9b8140836acdc60a197370819d13c8902b78ad5d
Mohammed Samy <mom118580@gmail.com>
2024-09-08 08:08:41 -0700

    App Colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff8685E7);
+  static const Color backgroundVColor = Color(0xff121212);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color redColor = Color(0xffFF4666);
+  static const Color greenColor = Color(0xff66CC41);
+  static const Color blueGreyColor = Color(0xff41CCA7);
+  static const Color blueColor = Color(0xff4181CC);
+  static const Color orangeColor = Color(0xffCC8441);
+  static const Color purpleColor = Color(0xff9741CC);
+  static const Color greyColor = Color(0xff979797);
+  static const Color deepGreyColor = Color(0xff424242);
+}
