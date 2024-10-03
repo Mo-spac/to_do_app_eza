9020c70a64f52e35904629a311d419aec1893f49
Mohammed Samy <mom118580@gmail.com>
2024-10-03 12:47:51 -0700

    Use date_picker_timeline package in Home Screen UI

--- lib/featured/task/presentation/screens/home_screen.dart
+++ lib/featured/task/presentation/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:to_do_app_ageeza/core/utils/app_colors.dart';
@@ -29,7 +30,25 @@ class HomeScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                       color: Colors.white,
                     ),
-              )
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              DatePicker(
+                DateTime.now(),
+                initialSelectedDate: DateTime.now(),
+                selectionColor: AppColors.primaryColor,
+                selectedTextColor: AppColors.whiteColor,
+                dateTextStyle: Theme.of(context).textTheme.displayMedium!,
+                monthTextStyle: Theme.of(context).textTheme.displayMedium!,
+                dayTextStyle: Theme.of(context).textTheme.displayMedium!,
+                onDateChange: (date) {
+                  // New date selected
+                  //   setState(() {
+                  //     _selectedValue = date;
+                  //   });
+                },
+              ),
             ],
           ),
         ),
