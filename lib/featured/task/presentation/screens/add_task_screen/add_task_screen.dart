a398b04dd99cfaab4c464563ef2e5af666e42313
Mohammed Samy <mom118580@gmail.com>
2024-10-04 02:46:30 -0700

    create Add Task Screen : appbar

--- lib/featured/task/presentation/screens/add_task_screen/add_task_screen.dart
+++ lib/featured/task/presentation/screens/add_task_screen/add_task_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app_ageeza/core/utils/app_colors.dart';
+import 'package:to_do_app_ageeza/core/utils/app_strings.dart';
 
 class AddTaskScreen extends StatelessWidget {
   const AddTaskScreen({super.key});
@@ -7,7 +9,24 @@ class AddTaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          elevation: 0.0,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_ios_new_outlined,
+              color: AppColors.whiteColor,
+            ),
+          ),
+          title: Text(
+            AppStrings.addTask,
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
+          centerTitle: false,
+        ),
       ),
     );
   }
